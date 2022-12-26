0df653d0fbd1f00221dd4d9f7557ba3cee7901aa
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2022-12-26 15:38:30 +0200

    remove changes

--- WEB-coursework-Site/Program.cs
+++ WEB-coursework-Site/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-//test
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {

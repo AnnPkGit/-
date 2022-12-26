18f037b715af0799c1b7bd9e45a288e7b69baf50
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2022-12-26 15:15:56 +0200

    Changes added for demonstration

--- WEB-coursework-Site/Program.cs
+++ WEB-coursework-Site/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//adding changes
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();

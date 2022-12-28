60ac58530167b2cc076de6942ede37bd1d0ec902
pallyh <pallyx11@gmail.com>
2022-12-28 16:53:43 +0200

    Готовая модель юзера

--- WEB-coursework-Site/Models/UserModel.cs
+++ WEB-coursework-Site/Models/UserModel.cs
@@ -3,5 +3,15 @@
     public class UserModel
     {
         public string RealName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string SecretQuestion { get; set; }
+
+        public IFormFile Avatar { get; set; }
     }
 }

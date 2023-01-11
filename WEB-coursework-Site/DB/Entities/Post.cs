59101c7ea18a9ff03874fae2c00cdf1834c52a99
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2023-01-11 08:33:59 +0200

    Создание Entity поста

---
+++ WEB-coursework-Site/DB/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace WEB_coursework_Site.DB.Entities
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public string? Text { get; set; }
+
+        public List<string>? Images { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Comments { get; set; }
+    }
+}

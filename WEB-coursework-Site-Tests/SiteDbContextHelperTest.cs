36b18a6c1306a2b0378a05a32af7ab2a7560e2a9
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2023-01-22 16:57:53 +0200

    добавлены юнит тесты для EntityValidator и SiteDbContextHelper

---
+++ WEB-coursework-Site-Tests/SiteDbContextHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using WEB_coursework_Site.DB.Context;
+using WEB_coursework_Site.DB.Validators;
+
+namespace WEB_coursework_Site_Tests
+{
+    internal class SiteDbContextHelperTest
+    {
+        private Mock<SiteDbcontext> _siteDbcontext;
+        private Mock<EntityValidator> _entityValidator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _siteDbcontext = new Mock<SiteDbcontext>(MockBehavior.Strict);
+            _entityValidator = new Mock<EntityValidator>(MockBehavior.Strict);
+        }
+
+        [Test]
+        public void ConstructorThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new SiteDbContextHelper(_siteDbcontext.Object, _entityValidator.Object, null));
+        }
+    }
+}
